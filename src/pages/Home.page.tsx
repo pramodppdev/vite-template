0b9c7981961aec1c66b07069f156ce3b643fbdd6
thealchimests <thealchemists.98@gmail.com>
2023-12-26 20:09:40 +0530

    problem with grid

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,15 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { DoubleNavbar } from '@/components/SideBar/DoubleNavbar';
+import { Subgrid } from '@/components/gird/grid';
 
 export function HomePage() {
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      {/* <Welcome />
+      <ColorSchemeToggle /> */}
+      {/* <DoubleNavbar/> */}
+      <Subgrid/>
     </>
   );
 }
